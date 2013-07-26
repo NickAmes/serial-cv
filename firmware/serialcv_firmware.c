3cad1e84153eb053c7a1d91858b6d45ef779ed2c
Nick Ames <nick@fetchmodus.org>
2013-07-26 11:59:34 -0700

    Copyright/Authorship notice.

---
+++ firmware/serialcv_firmware.c
@@ -0,0 +1,4 @@
+/* Firmware for Serial-CV converter board.
+ * http://www.fetchmodus.org/projects/serialcv/
+ * Written in 2013 by Nick Ames <nick@fetchmodus.org>.
+ * Placed in the Public Domain. */
