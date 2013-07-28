9f22fd743822a1381d87f4e60fd3fe2b0cca294c
Nick Ames <nick@fetchmodus.org>
2013-07-27 17:59:25 -0700

    Corrected bug causing the shutdown bit to be inadvertently set.

--- programs/serial-cv.c
+++ programs/serial-cv.c
@@ -90,7 +90,7 @@ int serialcv_voltage(float voltage){
 		if(voltage > vref)voltage = vref;
 		fraction = (voltage * 4096)/vref;
 		first = 0x01 | (fraction >> 4);
-		second = 0x3E | (fraction << 1);
+		second = 0x3E & (fraction << 1);
 	} else {
 		/* Shutdown the DAC. */
 		first = 0x01;
